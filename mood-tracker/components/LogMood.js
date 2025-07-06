b4b0f3350a5ab0f9cab437531a3671c63cbd5fa6
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-07-06 18:00:35 +0300

    Feat: Get User's Mood Selection

--- mood-tracker/components/LogMood.js
+++ mood-tracker/components/LogMood.js
@@ -1,4 +1,4 @@
-import { Button, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { Button, StyleSheet, Text, View, Image, TouchableOpacity, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -19,35 +19,50 @@ export const LogMood = () => {
 
 const EmojiIcons = () => (
   <View style={styles.emojiLayout}>
-    <TouchableOpacity>
+    <TouchableOpacity
+    style={styles.emojiIcon}
+    onPress={() => recordMood('happy')}
+    >
       <Image
       style={styles.emoji}
       source={require('../assets/happy.png')}
       />
       <Text>Happy</Text>
     </TouchableOpacity>
-    <TouchableOpacity>
+    <TouchableOpacity
+    style={styles.emojiIcon}
+    onPress={() => recordMood('neutral')}
+    >
       <Image
       style={styles.emoji}
       source={require('../assets/neutral.png')}
       />
       <Text>Neutral</Text>      
     </TouchableOpacity>
-    <TouchableOpacity>
+    <TouchableOpacity
+    style={styles.emojiIcon}
+    onPress={() => recordMood('sad')}
+    >
       <Image
       style={styles.emoji}
       source={require('../assets/sad.png')}
       />
       <Text>Sad</Text>
     </TouchableOpacity>
-    <TouchableOpacity>
+    <TouchableOpacity
+    style={styles.emojiIcon}
+    onPress={() => recordMood('angry')}
+    >
       <Image
       style={styles.emoji}
       source={require('../assets/angry.png')}
       />
       <Text>Angry</Text>
     </TouchableOpacity>
-    <TouchableOpacity>
+    <TouchableOpacity
+    style={styles.emojiIcon}
+    onPress={() => recordMood('tired')}
+    >
       <Image
       style={styles.emoji}
       source={require('../assets/tired.png')}
@@ -56,6 +71,10 @@ const EmojiIcons = () => (
     </TouchableOpacity>
   </View>
 )
+
+function recordMood(mood) {
+  Alert.alert(`Mood selected. You are ${mood} today.`)
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -72,4 +91,11 @@ const styles = StyleSheet.create({
     width: 150,
     height: 150,
   },
+  emojiIcon: {
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: '25%',
+    margin: 10,
+    alignItems: 'center'
+  }
 })
